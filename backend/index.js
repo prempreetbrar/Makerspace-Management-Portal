04f19c1863f64b1ba560d803b551cbc02dd34e0a
Prempreet Brar <prempreet.brar@ucalgary.ca>
2024-11-07 20:22:11 -0700

    prettier invoked on index.js

--- backend/index.js
+++ backend/index.js
@@ -5,27 +5,38 @@
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const seedDatabase = require("./seed");
-const userRoutes = require("./routes/users");
-const issueRoutes = require("./routes/issues");
-const equipmentRoutes = require("./routes/equipment");
-const bookingRoutes = require("./routes/bookings");
-const requestRoutes = require("./routes/requests");
+const cors = require('cors');
+const seedDatabase = require('./seed');
+const userRoutes = require('./routes/users');
+const issueRoutes = require('./routes/issues');
+const equipmentRoutes = require('./routes/equipment');
+const bookingRoutes = require('./routes/bookings');
+const requestRoutes = require('./routes/requests');
+const util = require('./controllers/util');
 const listenPort = 8080; // Must be the same as the LEFT part of the docker backend port. See the docker compose
 
 // localhost:{backend port} -> must match left half of line 8 in dockerfile;
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:8800", "http://frontend:80", "http://localhost:80", "http://frontend:8800", "http://backend:8080", "http://backend:5001", "http://localhost:8080", "http://localhost:5001"],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:8800',
+    'http://frontend:80',
+    'http://localhost:80',
+    'http://frontend:8800',
+    'http://backend:8080',
+    'http://backend:5001',
+    'http://localhost:8080',
+    'http://localhost:5001',
+  ],
 };
 app.use(cors(corsOptions));
 
 seedDatabase();
-app.use("/users", userRoutes); 
-app.use("/issues", issueRoutes);
-app.use("/equipment", equipmentRoutes);
-app.use("/bookings", bookingRoutes);
-app.use("/requests", requestRoutes);
+app.use('/users', userRoutes);
+app.use('/issues', issueRoutes);
+app.use('/equipment', equipmentRoutes);
+app.use('/bookings', bookingRoutes);
+app.use('/requests', requestRoutes);
 
 app.listen(listenPort, () => {
 console.log(`Server started on port ${listenPort}`);
