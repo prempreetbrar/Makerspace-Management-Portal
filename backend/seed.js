d1be0fcbf7dacc17ceea6ee45adfb9a19223ae4b
Prempreet Brar <prempreet.brar@ucalgary.ca>
2024-11-07 21:16:24 -0700

    prettify docker-compose and seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -5,15 +5,15 @@
  * the 'Add test data to tables' comment.
  */
 
-const sequelize = require("./config/database");
-const fs = require("fs");
-const path = require("path");
-const UserModel = require("./models/User");
-const IssueModel = require("./models/Issue");
-const EquipmentModel = require("./models/Equipment");
-const BookingModel = require("./models/Booking");
-const RequestModel = require("./models/Request");
-const AttachmentModel = require("./models/Attachment");
+const sequelize = require('./config/database');
+const fs = require('fs');
+const path = require('path');
+const UserModel = require('./models/User');
+const IssueModel = require('./models/Issue');
+const EquipmentModel = require('./models/Equipment');
+const BookingModel = require('./models/Booking');
+const RequestModel = require('./models/Request');
+const AttachmentModel = require('./models/Attachment');
 
 // Import models
 const User = UserModel(sequelize);
@@ -32,7 +32,7 @@ Object.keys(sequelize.models).forEach((modelName) => {
 
 // Sync the database and seed data
 // Set the clear = true to erase existing data from your database
-const seedDatabase = async (clear = true) => {
+const seedDatabase = async (clear = false) => {
   try {
     if (clear) {
       // Clear existing data
@@ -48,31 +48,31 @@ const seedDatabase = async (clear = true) => {
     if (userCount === 0) {
       await User.bulkCreate([
         {
-          email: "real_email1@email.com",
-          firstName: "Connor",
-          lastName: "McDavid",
-          userRole: "Premium",
+          email: 'real_email1@email.com',
+          firstName: 'Connor',
+          lastName: 'McDavid',
+          userRole: 'Premium',
           password:
-            "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", // SHA-256 Hash of "password"
+            '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', // SHA-256 Hash of "password"
         },
         {
-          email: "real_email2@email.com",
-          firstName: "Sidney",
-          lastName: "Crosby",
-          userRole: "Basic",
+          email: 'real_email2@email.com',
+          firstName: 'Sidney',
+          lastName: 'Crosby',
+          userRole: 'Basic',
           password:
-            "c0e21a8ff85153deac82fe7f09c0da1b3bd90ac0ae204e78d7148753b4363c03", // SHA-256 Hash of "wordpass"
+            'c0e21a8ff85153deac82fe7f09c0da1b3bd90ac0ae204e78d7148753b4363c03', // SHA-256 Hash of "wordpass"
         },
         {
-          email: "real_email3@email.com",
-          firstName: "Austin",
-          lastName: "Matthews",
-          userRole: "Admin",
+          email: 'real_email3@email.com',
+          firstName: 'Austin',
+          lastName: 'Matthews',
+          userRole: 'Admin',
           password:
-            "80d1159c872683756864281692bf8ffa330341cae85c8e188a2bf29edd7adbbe", // SHA-256 Hash of "extraSuperStrongP@s$W0Rd!"
+            '80d1159c872683756864281692bf8ffa330341cae85c8e188a2bf29edd7adbbe', // SHA-256 Hash of "extraSuperStrongP@s$W0Rd!"
         },
       ]);
-      console.log("Seeded user table");
+      console.log('Seeded user table');
     }
 
     // Equipment
@@ -81,28 +81,28 @@ const seedDatabase = async (clear = true) => {
       await Equipment.bulkCreate([
         {
           id: 1,
-          name: "3D Printer",
-          description: "This machine prints things in three dimensions!",
+          name: '3D Printer',
+          description: 'This machine prints things in three dimensions!',
           icon: fs.readFileSync(
-            path.join(__dirname, "/assets/icons", "3d_printer.png")
+            path.join(__dirname, '/assets/icons', '3d_printer.png')
           ),
-          equipmentStatus: "good",
+          equipmentStatus: 'good',
           isBookable: true,
           isPremium: true,
         },
         {
           id: 2,
-          name: "Stapler",
-          description: "Staples stuff",
+          name: 'Stapler',
+          description: 'Staples stuff',
           icon: fs.readFileSync(
-            path.join(__dirname, "/assets/icons", "stapler.png")
+            path.join(__dirname, '/assets/icons', 'stapler.png')
           ),
-          equipmentStatus: "really good",
+          equipmentStatus: 'really good',
           isBookable: false,
           isPremium: false,
         },
       ]);
-      console.log("Seeded equipment table");
+      console.log('Seeded equipment table');
     }
 
     // Issues (with relationships to Equipment by ID)
@@ -112,19 +112,19 @@ const seedDatabase = async (clear = true) => {
         {
           id: 1,
           equipmentID: 1, // Relates to the 3D Printer
-          description: "3D printer nozzle clogged",
+          description: '3D printer nozzle clogged',
           dateSubmitted: new Date(),
           issueStatus: false,
         },
         {
           id: 2,
           equipmentID: 2, // Relates to the Stapler
-          description: "Stapler out of staples",
+          description: 'Stapler out of staples',
           dateSubmitted: new Date(),
           issueStatus: true,
         },
       ]);
-      console.log("Seeded issue table");
+      console.log('Seeded issue table');
     }
 
     // Bookings (with relationships to Equipment and User by ID)
@@ -133,13 +133,13 @@ const seedDatabase = async (clear = true) => {
       await Booking.bulkCreate([
         {
           id: 1,
-          userEmail: "real_email1@email.com", // Relates to Connor McDavid
+          userEmail: 'real_email1@email.com', // Relates to Connor McDavid
           equipmentID: 1, // Relates to the 3D Printer
           bookingDateTime: new Date(),
           bookingDuration: 3,
         },
       ]);
-      console.log("Seeded booking table");
+      console.log('Seeded booking table');
     }
 
     // Requests (with relationships to Users by email)
@@ -148,20 +148,20 @@ const seedDatabase = async (clear = true) => {
       await Request.bulkCreate([
         {
           id: 1,
-          userEmail: "real_email1@email.com", // Relates to Connor McDavid
+          userEmail: 'real_email1@email.com', // Relates to Connor McDavid
           equipmentID: 1,
           description: "I'd like to use the 3D printer",
-          status: "approved",
+          status: 'approved',
         },
         {
           id: 2,
-          userEmail: "real_email3@email.com", // Relates to Austin Matthews
+          userEmail: 'real_email3@email.com', // Relates to Austin Matthews
           equipmentID: 2,
           description: "I'd like to use the stapler",
-          status: "pending",
+          status: 'pending',
         },
       ]);
-      console.log("Seeded request table");
+      console.log('Seeded request table');
     }
 
     // Attachments (with relationships to Requests by ID)
@@ -172,21 +172,21 @@ const seedDatabase = async (clear = true) => {
           id: 1,
           requestID: 1, // Relates to the first request
           file: fs.readFileSync(
-            path.join(__dirname, "/assets/attachments", "3d_printer_manual.pdf")
+            path.join(__dirname, '/assets/attachments', '3d_printer_manual.pdf')
           ),
         },
         {
           id: 2,
           requestID: 2, // Relates to the second request
           file: fs.readFileSync(
-            path.join(__dirname, "/assets/attachments", "stapler.jpg")
+            path.join(__dirname, '/assets/attachments', 'stapler.jpg')
           ),
         },
       ]);
-      console.log("Seeded attachment table");
+      console.log('Seeded attachment table');
     }
   } catch (error) {
-    console.error("Error seeding database:", error);
+    console.error('Error seeding database:', error);
   }
 };
 
