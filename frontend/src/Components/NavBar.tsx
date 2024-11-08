20a287cff26b9e855ab97ccd495b3e71473c77cd
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-11-08 11:49:43 -0700

    Added NavBar component

---
+++ frontend/src/Components/NavBar.tsx
@@ -0,0 +1,24 @@
+// This will probably need to take some props to change the displayed linked depending on the page
+
+import '../styles/navbar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        ✂️ {/* Logo Icon  will replace here*/}
+      </div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#reserve">Reserve Equipment</a></li>
+        <li><a href="#requests">View Requests</a></li>
+      </ul>
+      <div className="auth-buttons">
+        <button className="login-button">Log In</button>
+        <button className="create-account-button">Create Account</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -4,27 +4,13 @@ import React from 'react';
 import '../styles/style.css';
 import illustrationDesktop from '../assets/2.jpg';
 import illustrationMobile from '../assets/1.png';
+import NavBar from '../Components/NavBar.tsx';
+
 const Landing = () => {
   return (
     <div className='bg'>
       <div className="container">
-        {/* Navbar */}
-        <nav className="navbar">
-          <div className="logo-container">
-            ✂️ {/* Logo Icon  will replace here*/}
-          </div>
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#reserve">Reserve Equipment</a></li>
-            <li><a href="#requests">View Requests</a></li>
-          </ul>
-          <div className="auth-buttons">
-            <button className="login-button">Log In</button>
-            <button className="create-account-button">Create Account</button>
-          </div>
-        </nav>
-
-
+        <NavBar></NavBar>
         <div className="content">
           <div className="logo-container2">
             ✂️ {/* Logo Icon */}
