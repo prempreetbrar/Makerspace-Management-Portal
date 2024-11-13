0932a4d172be29824f7da2633b373eeb7b355723
Carter <cartermar211@gmail.com>
2024-11-13 14:02:34 -0700

    [minor] fix Requests.tsx after botched merge

--- frontend/src/hooks/UserProvider.tsx
+++ frontend/src/hooks/UserProvider.tsx
@@ -34,7 +34,6 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     const setUserByIndex = (index: number) => {
         setCurrentUserIndex(index % users.length);
     };
-
     return (
         <UserContext.Provider value={{ user: users[currentUserIndex], setUserByIndex }}>
             {children}
--- frontend/src/pages/Requests.tsx
+++ frontend/src/pages/Requests.tsx
@@ -2,7 +2,7 @@ import '../styles/requests/local.css';
 import * as React from 'react';
 import NavBar from '../Components/NavBar.tsx';
 import MainContainer from '../Components/MainContainer.tsx';
-import { Fab, Tab, Tabs, Stack, Typography } from '@mui/material';
+import { Fab, Tab, Tabs, Stack, Typography, Button, Card, CardContent } from '@mui/material';
 import { createTheme, styled, ThemeProvider, useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box';
 import TabPanel from '@mui/lab/TabPanel';
@@ -26,7 +26,13 @@ const Requests = () => {
         * Getting a state
     */}
     const { user, setUserByIndex } = useUser();
-    setUserByIndex(0); // basic user
+    const [currentUserIndex, setCurrentUserIndex] = React.useState(0);
+    const handleChangeUser = () => {
+        const nextIndex = currentUserIndex + 1 % 3;
+        setCurrentUserIndex(nextIndex);
+        setUserByIndex(nextIndex);
+    }
+
     const [value, setValue] = React.useState('Approved'); // this is the default state I assume
     const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
@@ -34,100 +40,99 @@ const Requests = () => {
 
     const randomList: Array<String> = ["Apples", "Bananas", "Oranges", "Celery", "Carrots", "Avocados", "Pineapples", "Mangoes", "Potatoes", "Tomatoes", "Beans"];
     return (
-        <div className='bg'>
-            <MainContainer>
-                {/* at some point, there should be a check for some sort of session token*/
-                /* also I don't know how to do media queries, so designing mobile first */}
-                <NavBar />
-                <ThemeProvider theme={theme}>
-                    <div className='page-content'>
-                        <TabContext value={value}>
-                            <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+        <MainContainer>
+            <NavBar />
+            <Button onClick={handleChangeUser}> Change User </Button>
+            <ThemeProvider theme={theme}>
+                <div className='page-content'>
+                    <TabContext value={value}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
 
-                                <Tab label="Approved" value="Approved" />
-                                <Tab label="Pending" value="Pending" />
-                                <Tab label="Denied" value="Denied" />
-
-                            </TabList>
-                            <TabPanel value="Approved">
-                                {/* TO DO: Make separate pages for each */}
-                                <Box sx={{ width: '100%' }}>
-                                    <Typography variant='h2' sx={{ padding: '30px' }}>
-                                        Approved Requests Tab <br />
-
-
-                                    </Typography>
-                                    <Stack spacing={3}>
-                                        {
-                                            randomList.map(
-                                                (item, index) =>
-                                                    <Typography key={index} variant='body2'> {item} </Typography>
-                                            )
-                                        }
-                                    </Stack>
-                                </Box>
-                            </TabPanel>
-                            <TabPanel value="Pending">
+                            <Tab label="Approved" value="Approved" />
+                            <Tab label="Pending" value="Pending" />
+                            <Tab label="Denied" value="Denied" />
+                        </TabList>
+                        <TabPanel value="Approved">
+                            {/* TO DO: Make separate pages for each */}
+                            <Box sx={{ width: '100%' }}>
+                                <Typography variant='h2' sx={{ padding: '30px' }}>
+                                    Approved Requests Tab <br />
+                                    {user.email}
+                                </Typography>
+                                <Stack spacing={3}>
+                                    {
+                                        randomList.map(
+                                            (item, index) =>
+                                                <Typography key={index} variant='body2'> {item} </Typography>
+                                        )
+                                    }
+                                </Stack>
+                            </Box>
+                        </TabPanel>
+                        <TabPanel value="Pending">
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                width: '100%'
+                            }}>
+                                <Typography variant='h2' sx={{ padding: '30px' }}>
+                                    Pending Requests Tab
+                                </Typography>
                                 <Box sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    width: '100%'
+                                    alignSelf: 'center',
+                                    alignContent: 'center',
                                 }}>
-                                    <Typography variant='h2' sx={{ padding: '30px' }}>
-                                        Pending Requests Tab
-                                    </Typography>
-                                    <Box sx={{
-                                        alignSelf: 'center',
-                                        alignContent: 'center',
-                                    }}>
-                                        <Stack spacing={3}>
-                                            {
-                                                randomList.map(
-                                                    (item, index) =>
-                                                        <Box key={index} bgcolor={theme.palette.secondary.light} sx=
-                                                            {{
-                                                                width: '80vw',
-                                                                height:
-                                                                {
-                                                                    xs: '100px',
-                                                                    md: '120px',
-                                                                },
-                                                                borderRadius: '20px'
-                                                            }}>
-                                                            <Typography key={index} variant='body2' sx={{
-                                                                fontWeight: 'bold',
-                                                                fontSize: '24pt',
-
-                                                            }} color={theme.palette.primary.contrastText}> {item} </Typography>
-                                                        </Box>
-                                                )
-                                            }
-
-                                        </Stack>
-                                    </Box>
-                                </Box>
-                            </TabPanel>
-                            <TabPanel value="Denied">
-                                <Box sx={{ width: '100%' }}>
-                                    <Typography variant='h2' sx={{ padding: '30px' }}>
-                                        Denied Requests Tab
-                                    </Typography>
                                     <Stack spacing={3}>
                                         {
-                                            randomList.map(
-                                                (item, index) =>
-                                                    <Typography key={index} variant='body2'> {item} </Typography>
+                                            randomList.map((item, index) =>
+                                                <Card key={index}
+                                                    sx={{
+                                                        backgroundColor: theme.palette.secondary.main,
+                                                        width: '80vw',
+                                                        height:
+                                                        {
+                                                            xs: '100px',
+                                                            md: '120px',
+                                                        },
+                                                        borderRadius: '20px'
+                                                    }}>
+                                                    <CardContent>
+                                                        <Typography key={index} variant='body2' sx={{
+                                                            color: theme.palette.secondary.contrastText,
+                                                            fontWeight: 'bold',
+                                                            fontSize: '24pt',
+                                                        }}> {item}
+                                                        </Typography>
+                                                    </CardContent>
+                                                </Card>
                                             )
                                         }
                                     </Stack>
                                 </Box>
-                            </TabPanel>
-                        </TabContext>
-                        <Fab />
-                    </div>
-                </ThemeProvider >
-            </MainContainer >
-        </div >
+                            </Box>
+                        </TabPanel>
+                        <TabPanel value="Denied">
+                            <Box sx={{ width: '100%' }}>
+                                <Typography variant='h2' sx={{ padding: '30px' }}>
+                                    Denied Requests Tab
+                                </Typography>
+                                <Stack spacing={3}>
+                                    {
+                                        randomList.map(
+                                            (item, index) =>
+                                                <Typography key={index} variant='body2'> {item} </Typography>
+                                        )
+                                    }
+                                </Stack>
+                            </Box>
+                        </TabPanel>
+                    </TabContext>
+                    <Fab />
+                </div>
+            </ThemeProvider >
+            {/* at some point, there should be a check for some sort of session token*/
+                /* also I don't know how to do media queries, so designing mobile first */}
+        </MainContainer >
     )
 }
 
