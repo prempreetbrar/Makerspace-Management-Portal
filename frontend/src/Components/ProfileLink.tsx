b973a018f053fdca6fe37ef48af02274dbf0e6c0
Prempreet Brar <prempreet.brar@ucalgary.ca>
2024-11-24 02:44:41 -0700

    logout functionality

--- frontend/src/Components/ProfileLink.tsx
+++ frontend/src/Components/ProfileLink.tsx
@@ -1,29 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '../styles/profile_link.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 
 const ProfileLink = () => {
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const { logout } = useContext(AuthContext)!;
+    const navigate = useNavigate();
 
-  const togglePopup = () => {
-    setIsPopupVisible((prev) => !prev);
-  };
+    const togglePopup = () => {
+        setIsPopupVisible((prev) => !prev);
+    };
 
-  return (
-    <div className="profile-link-container">
-      <a onClick={togglePopup}>
-        <img className="link-image" alt="React Logo" />
-      </a>
-      {isPopupVisible && (
-        <div className="popup-menu">
-          <ul>
-            <li><Link to="/profile" style={{textDecoration: 'none', color: 'black'}}>Profile</Link></li>
-            <li>Logout</li>
-          </ul>
+    const handleLogout = () => {
+        logout();
+        navigate('/');
+    };
+
+    return (
+        <div className="profile-link-container">
+            <a onClick={togglePopup}>
+                <img className="link-image" alt="React Logo" />
+            </a>
+            {isPopupVisible && (
+                <div className="popup-menu">
+                    <ul>
+                        <li>
+                            <Link
+                                to="/profile"
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'black',
+                                }}
+                            >
+                                Profile
+                            </Link>
+                        </li>
+                        <li
+                            onClick={handleLogout}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            Logout
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default ProfileLink;
