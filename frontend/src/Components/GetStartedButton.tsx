e856a58db6fcf600ba9f14c2ad681811fe325da5
Mehedi <mdabdullah.patwary@ucalgary.ca>
2024-12-05 03:49:03 -0700

    styled the get-started button

--- frontend/src/Components/GetStartedButton.tsx
+++ frontend/src/Components/GetStartedButton.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
 import '../styles/get_started_button.css';
+import {Button} from '@mui/material';
 interface GetStartedButtonProps
 {
     onClick: ()=>void
 }
 const GetStartedButton = ({onClick}:GetStartedButtonProps) => {
   return (
-    <button className="get-started-button" onClick={onClick}>Get Started</button>
+    <Button className="get-started-button" onClick={onClick} sx={{ backgroundColor: 'white', color: 'black', border: '1px solid #ddd', boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.2)', marginRight: '8px',
+    textTransform: 'none',
+    '&:hover': {
+        backgroundColor: '#f1f1f1',
+    },width: '100px', borderRadius: 2,display: 'none',
+    '@media (min-width: 769px)': {
+      display: 'inline-block',
+    },
+  }}>Get Started</Button>
   );
 };
 
